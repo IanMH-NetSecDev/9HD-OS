09fb7a4b6f6011fe392c64f180becdaa9348769c
fart-maker <anthengoodwin1@gmail.com>
2026-01-07 19:54:01 -0900

    Add files via upload
    testing formatting on line 206

--- Assets/9HDOS_Installer_v0.1_Local.sh
+++ Assets/9HDOS_Installer_v0.1_Local.sh
@@ -203,6 +203,9 @@ if [ -n "selected_disk" ]; then
 
 if (whiptail --yesno "are you sure you want to format /dev/$selected_disk?" 10 50); then
 echo "formatting /dev/$selected_disk... please wait"
+sudo mkfs ext4 /dev/$selected_disk;
+
+
    
 
 
